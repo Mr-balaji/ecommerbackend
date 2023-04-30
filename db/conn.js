773a4c8172573856b5efe1f9c600e17330032f6d
balaji <bdevtar@hexalytics.com>
2023-04-30 16:16:52 +0530

    fixing

--- db/conn.js
+++ db/conn.js
@@ -8,6 +8,7 @@ moongose.connect(process.env.MONGODB_URL,
     {useNewUrlParser:true,useUnifiedTopology: true ,family:4}
     ).then(()=>{
         console.log("connection successfull");
+        console.log(typeof(process.env.MONGODB_URL));
     }).catch((err)=>{
         console.log(err);
     })
